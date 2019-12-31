d86a41b61a7e6b7517a642515a4a8366ebdae162
Brais Gabín <braisgabin@gmail.com>
2019-12-31 14:07:49 +0100

    Edit migrate script to suit the rxjava3 migration

--- migrate.sh
+++ migrate.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
-# I've found that the "Migrate to AndroidX" converter in Android Studio doesn't work very
-# well, so I wrote my own script to do the simple job of converting package names.
+# Copyright 2018 Dan Lew
+# Copyright 2020 Brais Gabín
 #
-# You can download a CSV of package names here: https://developer.android.com/topic/libraries/support-library/downloads/androidx-class-mapping.csv
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# It'll run faster on a clean build because then there are fewer files to scan over.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Uses `gsed` because I'm on a Mac. Can easily replace with `sed` if you don't have `gsed`.
-# 
-# This isn't perfect; it won't find every conversion issue. You break it you buy it. Viewer discretion is advised.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-MAPPING_FILE=path/to/androidx-class-mapping.csv
-PROJECT_DIR=path/to/android/project
+if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+  echo "Usage: $0 [DIRECTORY]" >&2
+  exit 1
+fi
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+MAPPING_FILE=$SCRIPTPATH/rxjava3-class-mapping.csv
 
 replace=""
 while IFS=, read -r from to
@@ -20,4 +29,4 @@ do
 	replace+="; s/$from/$to/g"
 done <<< "$(cat $MAPPING_FILE)"
 
-find $PROJECT_DIR \( -name "*.kt" -o -name "*.java" -o -name "*.xml" \) -type f -not -path '*/\.git*' -print0 | xargs -0 gsed -i "$replace"
+find $1 \( -name "*.kt" -o -name "*.java" \) -type f -not -path '*/\.git*' -print0 | xargs -0 sed -i '' "$replace"
